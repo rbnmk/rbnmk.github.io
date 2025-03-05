26da6aba31ccf4cfb90eb09e246decf1a39e8daa
Robin Makkus <robin.makkus@inspark.nl>
2025-03-05 08:54:02 +0100

    Replace Facebook sharing link with Bluesky sharing link in social media hooks

--- hooks/socialmedia.py
+++ hooks/socialmedia.py
@@ -3,7 +3,7 @@ import urllib.parse
 import re
 
 x_intent = "https://x.com/intent/tweet"
-fb_sharer = "https://www.facebook.com/sharer/sharer.php"
+bluesky_sharer = "https://bsky.app/intent/compose"
 include = re.compile(r"blog/[1-9].*")
 
 def on_page_markdown(markdown, **kwargs):
@@ -17,5 +17,5 @@ def on_page_markdown(markdown, **kwargs):
 
     return markdown + dedent(f"""
     [Share on :simple-x:]({x_intent}?text={page_title}&url={page_url}){{ .md-button }}
-    [Share on :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button }}
+    [Share on :simple-bluesky:]({bluesky_sharer}?u={page_url}){{ .md-button }}
     """)
